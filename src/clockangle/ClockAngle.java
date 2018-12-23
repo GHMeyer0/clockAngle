c245fb47ef3ac8a588b364bac7ec9f0cc21ae545
Gabriel Helko Meyer <ghmeyer0@gmail.com>
2018-12-23 20:53:06 -0200

    Alterado projet

--- src/clockangle/ClockAngle.java
+++ src/clockangle/ClockAngle.java
@@ -6,6 +6,8 @@ import java.util.Locale;
 import java.util.Scanner;
 import java.util.TimeZone;
 
+import com.sun.glass.ui.Pixels.Format;
+
 /**
  *
  * @author ghmeyer0
@@ -17,12 +19,16 @@ public class ClockAngle {
      */
 
     //variaveis
-    int hora = 0, minuto = 0, angulo = 0;
+    
     
     public static void main(String[] args) 
     {
-        
-        GregorianCalendar gc = new GregorianCalendar();
+
+        Calendar cal = Calendar.getInstance(); 
+        SimpleDateFormat hora = new SimpleDateFormat("hh");
+        SimpleDateFormat minuto = new SimpleDateFormat("mm");
+
+        long angulo;
         Scanner in = new Scanner(System.in);
         
         System.out.println("Informe a hora e os minutos com espaço, ou deixe em branco para utilizar a hora atual: ");
@@ -32,7 +38,9 @@ public class ClockAngle {
         // Codição para pegar a hora atual caso não seja informado nenhum horario
         if (hora == 0 && minuto == 0)
         {
-          gc.getTime();  
+            gc.get(gc.HOUR);
+            System.out.println(gc.HOUR + "::::" + gc.MINUTE); 
+
         } 
         // caso seja informado o horario, seta o mesmo no gc
         else 
@@ -40,7 +48,7 @@ public class ClockAngle {
             gc.set(gc.HOUR,hora);
             gc.set(gc.MINUTE,minuto);
         }
-
+        System.out.println(hora + ":" + minuto);
         //chama o metodo que calcula o algulo entre o ponteiro de minutos e horas
         angulo = retornaAnguloRelogio(gc);
         System.out.println(hora + ":" + minuto); 
@@ -48,16 +56,16 @@ public class ClockAngle {
     }
 
     //Metodo para calcular o algulo
-    static public int retornaAnguloRelogio(GregorianCalendar time)
+    static public long retornaAnguloRelogio(GregorianCalendar time)
     {
         //Calcula posição do ponteiro de hora
         int angHora = time.get(Calendar.HOUR)*30; 
 
         //calcula possição do ponteiro de minuto
-        int angMinuto = time.get(Calendar.MINUTE);*6;
+        int angMinuto = time.get(Calendar.MINUTE)*6;
 
         //calcula angulo
-        int ang = angHora-angMinuto;
+        long ang = angHora-angMinuto;
         return ang;
     };    
     
