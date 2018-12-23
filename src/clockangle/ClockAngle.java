f9571ced51eb143d7cbb28b7a9118c33023f9c79
Gabriel Helko Meyer <ghmeyer0@gmail.com>
2018-12-23 12:19:02 -0200

    Adicionado comentarios

--- src/clockangle/ClockAngle.java
+++ src/clockangle/ClockAngle.java
@@ -15,35 +15,48 @@ public class ClockAngle {
     /**
      * @param args the command line arguments
      */
+
+    //variaveis
+    int hora = 0, minuto = 0, angulo = 0;
+    
     public static void main(String[] args) 
     {
-        int hora = 0, minuto = 0, angulo;
+        
         GregorianCalendar gc = new GregorianCalendar();
         Scanner in = new Scanner(System.in);
         
         System.out.println("Informe a hora e os minutos com espaço, ou deixe em branco para utilizar a hora atual: ");
         hora = in.nextInt();
         minuto = in.nextInt();
+
+        // Codição para pegar a hora atual caso não seja informado nenhum horario
         if (hora == 0 && minuto == 0)
         {
           gc.getTime();  
         } 
+        // caso seja informado o horario, seta o mesmo no gc
         else 
         {
             gc.set(gc.HOUR,hora);
             gc.set(gc.MINUTE,minuto);
         }
+
+        //chama o metodo que calcula o algulo entre o ponteiro de minutos e horas
         angulo = retornaAnguloRelogio(gc);
         System.out.println(hora + ":" + minuto); 
-
+        System.out.println(angulo); 
     }
+
+    //Metodo para calcular o algulo
     static public int retornaAnguloRelogio(GregorianCalendar time)
     {
-        int hora = time.get(Calendar.HOUR_OF_DAY);
-        int minuto = time.get(Calendar.MINUTE);
-        int angHora = hora*30;
-        int angMinuto = minuto*6;
-        
+        //Calcula posição do ponteiro de hora
+        int angHora = time.get(Calendar.HOUR)*30; 
+
+        //calcula possição do ponteiro de minuto
+        int angMinuto = time.get(Calendar.MINUTE);*6;
+
+        //calcula angulo
         int ang = angHora-angMinuto;
         return ang;
     };    
