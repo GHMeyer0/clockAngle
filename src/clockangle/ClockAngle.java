60301bbf74619b1ca7d1a2e7819b52b16237664b
Gabriel Helko Meyer <ghmeyer0@gmail.com>
2018-12-23 23:48:55 -0200

    Projeto finalizado

--- src/clockangle/ClockAngle.java
+++ src/clockangle/ClockAngle.java
@@ -6,8 +6,6 @@ import java.util.Locale;
 import java.util.Scanner;
 import java.util.TimeZone;
 
-import com.sun.glass.ui.Pixels.Format;
-
 /**
  *
  * @author ghmeyer0
@@ -17,72 +15,62 @@ public class ClockAngle {
     /**
      * @param args the command line arguments
      */
-<<<<<<< HEAD
 
-    //variaveis
-    
     
     public static void main(String[] args) 
     {
-
-        Calendar cal = Calendar.getInstance(); 
-        SimpleDateFormat hora = new SimpleDateFormat("hh");
-        SimpleDateFormat minuto = new SimpleDateFormat("mm");
-
+        //variaveis
+        int hora, minuto, opc;
         long angulo;
-=======
-    public static void main(String[] args) 
-    {
-        int hora = 0, minuto = 0, angulo;
-        GregorianCalendar gc = new GregorianCalendar();
->>>>>>> parent of f9571ce... Adicionado comentarios
+        Calendar gc = new GregorianCalendar();
         Scanner in = new Scanner(System.in);
+        Date date = new Date();
         
-        System.out.println("Informe a hora e os minutos com espaço, ou deixe em branco para utilizar a hora atual: ");
-        hora = in.nextInt();
-        minuto = in.nextInt();
-        if (hora == 0 && minuto == 0)
-        {
-            gc.get(gc.HOUR);
-            System.out.println(gc.HOUR + "::::" + gc.MINUTE); 
+        // Menu de decisão
+        System.out.println("/======================================================\\");
+        System.out.println("| Calcular o angulo da hora atual ou informar uma hora?|");
+        System.out.println("|         1 - Hora atual | 2 - Informar Hora           |");
+        System.out.println("\\======================================================/");
+        opc = in.nextInt();
 
-        } 
-        else 
-        {
+        // Setar hora atual
+        if(opc == 1){
+            gc.set(gc.HOUR, date.getHours());
+            gc.set(gc.MINUTE, date.getMinutes());
+        }
+
+        // Setar hora definida pelo usuario
+        else if( opc == 2){
+            System.out.println("| Informe a Hora e os minutos com espaço. ex: 22 59 |");
+            hora = in.nextInt();
+            minuto = in.nextInt();
             gc.set(gc.HOUR,hora);
             gc.set(gc.MINUTE,minuto);
         }
-<<<<<<< HEAD
-        System.out.println(hora + ":" + minuto);
+        else{
+            System.out.println("Opção Invalida");
+            System.exit(opc);
+        }
         //chama o metodo que calcula o algulo entre o ponteiro de minutos e horas
-=======
->>>>>>> parent of f9571ce... Adicionado comentarios
         angulo = retornaAnguloRelogio(gc);
-        System.out.println(hora + ":" + minuto); 
+        System.out.println("O Angulo entre os ponteiros é de: "+ angulo); 
+    }
 
-<<<<<<< HEAD
     //Metodo para calcular o algulo
-    static public long retornaAnguloRelogio(GregorianCalendar time)
+    static public long retornaAnguloRelogio(Calendar horario)
     {
         //Calcula posição do ponteiro de hora
-        int angHora = time.get(Calendar.HOUR)*30; 
-
+        int angHora = horario.get(horario.HOUR)*30; 
         //calcula possição do ponteiro de minuto
-        int angMinuto = time.get(Calendar.MINUTE)*6;
-
+        int angMinuto = horario.get(horario.MINUTE)*6;
+        long ang;
         //calcula angulo
-        long ang = angHora-angMinuto;
-=======
-    }
-    static public int retornaAnguloRelogio(GregorianCalendar time)
-    {
-        int hora = time.get(Calendar.HOUR_OF_DAY);
-        int minuto = time.get(Calendar.MINUTE);
-        int angHora = hora*30;
-        int angMinuto = minuto*6;
-        
-        int ang = angHora-angMinuto;
->>>>>>> parent of f9571ce... Adicionado comentarios
+        if (angHora >= angMinuto){
+            ang = angHora-angMinuto;    
+        }
+        else{
+            ang = angMinuto - angHora;
+        }
         return ang;
     };    
     
